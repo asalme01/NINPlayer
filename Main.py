93f99bfaa61bc88b31955e6efae2de2455f6732a
Alberto Salmeron <asalme01@calpoly.edu>
2018-06-24 12:04:32 -0700

    Main file contains code for window, not currently working

--- Main.py
+++ Main.py
@@ -1,1 +1,25 @@
-#Main file
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class Window(QScrollArea):
+    def __init__(self):
+        super(Window, self).__init__()
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        layout.setAlignment(Qt.AlignCenter)
+        for index in range(100):
+            layout.addWidget(QLabel('Label %02d' % index))
+        self.setWidget(widget)
+        self.setWidgetResizable(True)
+
+
+def main():
+    app = QApplication(sys.argv)
+    wind = Window()
+
+
+if __name__ == '__main__':
+    main()
